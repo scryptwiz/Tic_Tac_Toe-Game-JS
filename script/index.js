50b85c3f476bdd563bcf2d40ef7ad52b9affaea5
Kelvin Ajayi <kelvide7@gmail.com>
2022-07-10 19:36:07 +0100

    Initial commit

---
+++ script/index.js
@@ -0,0 +1,8 @@
+const playWithBtn = () => {
+    document.getElementById('mid_choose').style.display="none";
+    document.getElementById('pick_side').style.display="flex";
+}
+const start = () => {
+    document.getElementById('pick_side').style.display="none";
+    document.getElementById('game_cont').style.display="flex";
+}
