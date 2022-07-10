3caaaf8de68e2a8313b517af459470b09412df70
Kelvin Ajayi <kelvide7@gmail.com>
2022-07-11 00:18:36 +0100

    Little Edits

--- script/index.js
+++ script/index.js
@@ -6,3 +6,7 @@ const start = () => {
     document.getElementById('pick_side').style.display="none";
     document.getElementById('game_cont').style.display="flex";
 }
+
+// Game Start
+let cells = document.getElementsByClassName('cell');
+console.log(Array.from(cells));x
